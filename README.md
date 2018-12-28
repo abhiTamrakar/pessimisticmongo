893d061971af8e6f88e27bd70fcbab9d524ce880
abhiTamrakar <abhishek.tamrakar08@gmail.com>
2018-12-28 09:33:42 -0800

    updated the code

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Pessimistic lock implementation for mongoengine, using  a very simple logic
 
 `Inspired by mongolock and pessimistic locks concept.`
 
+Read more about the implementation [here](https://medium.com/@abhishek.tamrakar/implementing-pessimistic-locks-in-mongodb-8f3fbe2ddfa9)
+
 ## usage
 ```
 from mongoEngineLock import mongoEngineLock, MongoLockTimeout
@@ -10,6 +12,14 @@ from mongoEngineLock import mongoEngineLock, MongoLockTimeout
 lock = mongoEngineLock('<db..name>', poll=1, timeout=40, retries=15)
 
 with lock(owner):
+   try:
+      <..do something here..>
+   except Exception as e:
+      raise e
+
+# or use with conditional statements
+
+if lock(owner):
    try:
       <..do something here..>
    except Exception as e:
@@ -23,7 +33,7 @@ with lock(owner):
 **retries**: number of retries before timeout.
 
 ##### Instance of the class takes exactly 1 parameter: 
-**owner**: owner of the lock.
+**owner**: owner of the lock or collection name(in case there are more than one collections to be managed in a database).
 
 ## misc
 * Try to use a smaller polling interval or interval based on time taken to execute the query, in case of bigger DB size.
--- mongoEngineLock.py
+++ mongoEngineLock.py
@@ -1,6 +1,17 @@
 """
-Implement pessimistic lock using mongoengine.
+  Implementation of pessimistic lock using mongoengine.
 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 import time
 from bson import json_util
@@ -46,7 +57,7 @@ class mongoEngineLock(object):
     @contextlib.contextmanager
     def __call__(self, owner):
         if not self.lock(owner):
-           data = self._get_lockinfo()
+           data = self.getLockinfo(owner)
            data = json_util.loads(data)
            status = data[0]
            raise MongoLockTimeout(
@@ -57,14 +68,14 @@ class mongoEngineLock(object):
         try:
            yield
         finally:
-           self.release() 
+           self.release(owner) 
 
     def lock(self, owner):
-        _retry_step = 1
+        retry_step = 1
         _now = datetime.utcnow()
         while True:
            try:
-              if not self.isLocked():
+              if not self.isLocked(owner):
                  data = Locks(
                     ts=_now,
                     owner=owner,
@@ -77,23 +88,23 @@ class mongoEngineLock(object):
                  raise MongoCollectionLocked
 
            except MongoCollectionLocked:
-              print ('rt:%d, rts: %d, now: %s', _retry_step, self.retries, _now)
-              if _retry_step == self.retries or datetime.utcnow() >= _now + timedelta(seconds=self.timeout):
+              print ('rt:%d, rts: %d, now: %s', retry_step, self.retries, _now)
+              if retry_step == self.retries or datetime.utcnow() >= _now + timedelta(seconds=self.timeout):
                  return False
 
-              _retry_step+=1
+              retry_step+=1
               time.sleep(self.poll)
 
-    def isLocked(self):
-        status = Locks.objects(locked__exact=True).count()
+    def isLocked(self, owner):
+        status = Locks.objects(owner__exact=owner).count()
 
         return True if status == 1 else False
 
-    def _get_lockinfo(self):
+    def getLockinfo(self, owner):
 
-        return Locks.objects(locked__exact=True).to_json()
+        return Locks.objects(owner__exact=owner).to_json()
 
-    def release(self):
-        status = Locks.objects(locked__exact=True).delete()
+    def release(self, owner):
+        status = Locks.objects(owner__exact=owner).delete()
 
         return True if status == 1 else False
